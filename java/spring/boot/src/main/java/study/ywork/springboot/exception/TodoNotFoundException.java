7d7d0be10b618b4e8db6e4cd6fd2051f55949378
y2ghost <snrtcel@126.com>
2021-01-07 00:19:39 +0800

    重构boot代码，从头梳理

--- java/spring/boot/src/main/java/study/ywork/springboot/exception/TodoNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package study.ywork.springboot.exception;
-
-public class TodoNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 100001L;
-
-    public TodoNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- java/spring/boot/src/main/resources/db/migration/h2/V1.2__AddData.sql
+++
@@ -1,3 +0,0 @@
-insert into users(id, name, email) values(1,'yy1','yy1@126.com');
-insert into users(id, name, email) values(2,'yy2','yy2@126.com');
-insert into users(id, name, email) values(3,'yy3','yy3@126.com');
